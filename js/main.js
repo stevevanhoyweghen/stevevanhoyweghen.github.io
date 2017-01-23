f7f3924f27b44e3e8d7190c8217c8d1ab1407df3
stevevanhoyweghen <svhoyw@gmail.com>
2017-01-23 22:52:02 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,43 +2,23 @@ $(document).ready(function(){
 	$("#menu_toggle_large").click(function(){
 		$("#content").slideToggle("slow");
 	});
+
 	$("#menu_toggle_small").click(function(){
 		$("#content").slideToggle("slow");
 		($("#header_title_small").css("display") == "none" ? $("#header_title_small").show() : $("#header_title_small").hide());
 	});
-/*
-	$(window).resize(function(){
-		($("#menu_toggle").css("display") == "none" ? $("#x").show() : $("#x").hide());
-		($("#menu_toggle").css("display") == "none" ? $("#xx").show() : $("#xx").hide());
-	});
 
-	$("#textToggle").text("\u25bc");
-	$("#textToggle").click(function(){
-		$(".text").slideToggle("fast");
-		($("#textToggle").text() == "\u25bc" ? $("#textToggle").text("\u25ba") : $("#textToggle").text("\u25bc"));
-	});
-*/
-	function justifiedGallery() {
-		$('.lightbox').justifiedGallery({
-			captionSettings	: { animationDuration: 500, visibleOpacity: (isNaN(window.globals.defaultHideCaption) ? 0.6 : 0.0), nonVisibleOpacity: (isNaN(window.globals.defaultHideCaption) ? 0.0 : 0.6) },
-			rowHeight       : (isNaN(window.globals.justifiedGalleryRowHeight) ? 200 : window.globals.justifiedGalleryRowHeight),
-			lastRow         : (($(document).width() <= 800) ? 'justify' : 'nojustify'),
-			margins	        : 3,
-			rel             : 'gallery1',
-			refreshSensitivity	:	7,
-			refreshTime     : 800,
-			fixedheight		: true
-		}).on('jg.complete', function() {
-			$(this).find('a').simpleLightbox({
-				captionPosition:'outside'
-			});
+	$('.lightbox').justifiedGallery({
+		captionSettings	: { animationDuration: 500, visibleOpacity: (isNaN(window.globals.defaultHideCaption) ? 0.6 : 0.0), nonVisibleOpacity: (isNaN(window.globals.defaultHideCaption) ? 0.0 : 0.6) },
+		rowHeight       : (isNaN(window.globals.justifiedGalleryRowHeight) ? 200 : window.globals.justifiedGalleryRowHeight),
+		justifyThreshold: 0.55,
+		margins	        : 3,
+		rel             : 'gallery1',
+		fixedheight		: true
+	}).on('jg.complete', function() {
+		$(this).find('a').simpleLightbox({
+			captionPosition:'outside'
 		});
-	}
-
-	justifiedGallery();
-	
-	$(window).resize(function(){
-		justifiedGallery();
 	});
 
 	$(".lightbox > a > img").mouseenter(function() {
