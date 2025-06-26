7836f25bc676a52f9ccab0caa4ce466704733c5a
stevevanhoyweghen <steve.van.hoyweghen@proton.me>
2025-06-26 19:17:18 +0200

    Add biomorphic breath #1
    General update + add biomorphic breath #1 page

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
-https://stevevanhoyweghen.github.io
+# stevevanhoyweghen.github.io
 
 Steve Van Hoyweghen his photo portfolio on GitHub.
 
-Most content is generated with a generator I wrote. The exif and itpc data from the pictures are used to generate the site. I will share the implementation once all proves to be production ready.
+Most content is generated with a generator I wrote.
+The exif and itpc data from the pictures are used to generate the site.
+I will share the implementation once all proves to be production ready.
 
 Some background
 
-I have established a complete workflow covering: Lightroom -> exif/itpc cleanup -> thumbnail generation -> page generation, on a Windows computer making use of Python, Jinja2, VIPS, and exiftool. Since all these tools are available on linux and Mac as well, porting the workflow to another OS will be easy.
+I have established a complete workflow covering: Lightroom -> exif/itpc cleanup -> thumbnail generation -> page generation,
+on a Windows computer making use of Python, Jinja2, VIPS, and exiftool. Since all these tools are available on linux and Mac as well,
+porting the workflow to another OS will be easy.
 
 More pictures will be added later. For the impatient: https://stevevanhoyweghen.wordpress.com/
--- js/main.js
+++ js/main.js
@@ -1,33 +1,62 @@
 $(document).ready(function(){
-	$("#menu_toggle_large").click(function(){
-		$("#content").slideToggle("slow");
+	// OK alert('a1');
+	$(".menu_toggle").click(function(){
+		alert('a2');
+		$("#menu").slideToggle("slow");
 	});
 
-	$("#menu_toggle_small").click(function(){
-		$("#content").slideToggle("slow");
-		($("#header_title_small").css("display") == "none" ? $("#header_title_small").show() : $("#header_title_small").hide());
+	$(window).resize(function(){
+		//OK alert('a3');
+		($("#menuToggle").css("display") == "none" ? $("#menu").show() : $("#menu").hide());
 	});
 
+	$("#textToggle").text("\u25bc");
+	$("#textToggle").click(function(){
+		$(".text").slideToggle("fast");
+		($("#textToggle").text() == "\u25bc" ? $("#textToggle").text("\u25ba") : $("#textToggle").text("\u25bc"));
+	});
+
+	//var simpleLightbox = new SimpleLightbox('.lightbox a', {captionPosition:'bottom', captionDelay:1000});
+
+	
 	$('.lightbox').justifiedGallery({
 		captionSettings	: { animationDuration: 500, visibleOpacity: (isNaN(window.globals.defaultHideCaption) ? 0.6 : 0.0), nonVisibleOpacity: (isNaN(window.globals.defaultHideCaption) ? 0.0 : 0.6) },
 		rowHeight       : (isNaN(window.globals.justifiedGalleryRowHeight) ? 200 : window.globals.justifiedGalleryRowHeight),
-		justifyThreshold: 0.55,
+		lastRow         : 'nojustify',
 		margins	        : 3,
 		rel             : 'gallery1',
-		fixedheight		: true
+		refreshSensitivity	:	5,
+		refreshTime     : 600
+	})/*.on('jg.complete', function() {
+		$(this).find('a').simpleLightbox({
+			captionPosition:'outside'
+		});
+	})*/;
+
+
+/*
+	$('.lightbox').justifiedGallery({
+		captionSettings	: { animationDuration: 500, visibleOpacity: (isNaN(window.globals.defaultHideCaption) ? 0.6 : 0.0), nonVisibleOpacity: (isNaN(window.globals.defaultHideCaption) ? 0.0 : 0.6) },
+		rowHeight       : (isNaN(window.globals.justifiedGalleryRowHeight) ? 200 : window.globals.justifiedGalleryRowHeight),
+		lastRow         : 'nojustify',
+		margins	        : 3,
+		rel             : 'gallery1',
+		refreshSensitivity	:	5,
+		refreshTime     : 600 //!!!!!!!!!!!!,
+		//!!!!!!!!!!!! fixedheight		: true
 	}).on('jg.complete', function() {
 		$(this).find('a').simpleLightbox({
 			captionPosition:'outside'
 		});
 	});
-
+*/
 	$(".lightbox > a > img").mouseenter(function() {
 		$(this).animate({
-			height: '-=3px',
-			width: '-=3px'
-		}, 200).animate({
 			height: '+=3px',
 			width: '+=3px'
+		}, 200).animate({
+			height: '-=3px',
+			width: '-=3px'
 		},100);
 	});
 });
